9f6647181e3265123bae983149f11d96c178041b
rafi1903139 <66672478+rafi1903139@users.noreply.github.com>
2024-11-27 08:51:38 +0600

    bug fixed

--- models_doc.py
+++ models_doc.py
@@ -203,7 +203,7 @@ def calc_coeff(iter_num, high=1.0, low=0.0, alpha=10.0, max_iter=10000.0):
     Returns:
         float: A calculated coefficient in the range [low, high].
     """
-    return np.float(2.0 * (high - low) / (1.0 + np.exp(-alpha * iter_num / max_iter)) - (high - low) + low)
+    return np.float64(2.0 * (high - low) / (1.0 + np.exp(-alpha * iter_num / max_iter)) - (high - low) + low)
 
 def grl_hook(coeff):
     """
